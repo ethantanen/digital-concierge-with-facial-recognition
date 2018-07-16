eef4620fe9796d4716d4296c2c93a3f4f347a0f2
theheuman <theheuman@users.noreply.github.com>
2018-07-16 15:19:44 -0400

    Update slackUtilities.js

--- backend/utilities/slackUtilities.js
+++ backend/utilities/slackUtilities.js
@@ -1,26 +1,26 @@
 function postMessage(var channel, var text) {
-			var apiUrl = "https://slack.com/api/chat.postMessage";
-			var token = "xoxp-366481641941-370889279687-397840544053-05454a11ce6593938ae9b8026f248354";
-			var actualToken = "Bearer " + token;
+	var apiUrl = "https://slack.com/api/chat.postMessage";
+	var token = "xoxp-366481641941-370889279687-397840544053-05454a11ce6593938ae9b8026f248354";
+	var actualToken = "Bearer " + token;
+	
 			
-			
-			 $.ajax({                   
-    				data: {
-        			"token": token,
+	$.ajax({                   
+    		data: {
+        		"token": token,
        			"channel": channel,
-        			"text": text,
+        		"text": text,
        			"as_user":"false"
-   				},                      
-    				dataType: 'text',
-   				type: 'POST',           
-   				url: apiUrl,
-    				error: function(xhr,status,error){              
-        				console.log("error: " + error);
-    				},
-    				success: function(data) {
-        				console.log("result: " + data);
-    				}
-			});
+   		},                      
+    		dataType: 'text',
+   		type: 'POST',           
+   		url: apiUrl,
+    		error: function(xhr,status,error){              
+        		console.log("error: " + error);
+    		},
+    		success: function(data) {
+        		console.log("result: " + data);
+    		}
+	});
 }
 
 module.exports={postMessage:postMessage};
